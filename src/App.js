c2cc4b57d77a41f7cc78e086c6b1a0bf7f8c4312
lalitkumawat1m <lalitkumawat1129@gmail.com>
2022-12-24 05:26:46 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import TodoList from './component/TodoList';
+
+function App() {
+  return (
+   <div className='todo-app'>
+   <TodoList/>
+   </div>
+  );
+}
+
+export default App;
